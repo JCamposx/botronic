4b7c8408213d9e826857060f3b5ccf8bad9f5f3a
Jesus Campos <jesus.campos123@icloud.com>
2022-09-10 02:01:45 -0500

    Añadido autenticación login y registro

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+import '../css/app.css'
+
+const navbar = document.querySelector(".navbar");
+const welcome = document.querySelector(".welcome");
+const navbarToggle = document.querySelector("#navbarSupportedContent");
+
+const resizeBakgroundImg = () => {
+  const height = window.innerHeight - navbar.clientHeight;
+  welcome.style.height = `${height}px`;
+};
+
+navbarToggle.ontransitionend = resizeBakgroundImg;
+navbarToggle.ontransitionstart = resizeBakgroundImg;
+window.onresize = resizeBakgroundImg;
+window.onload = resizeBakgroundImg;
