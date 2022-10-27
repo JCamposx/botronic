f71ade5263d1a6d436a2b7d3184526f7806e7d52
Jesus Campos <jesus.campos123@icloud.com>
2022-10-26 23:12:26 -0500

    Añadiendo timestamps a los seeders

--- database/seeders/BotSeeder.php
+++ database/seeders/BotSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -17,6 +18,8 @@ class BotSeeder extends Seeder
     public function run()
     {
         for ($i = 1; $i <= 13; $i++) {
+            $date = Carbon::now();
+
             DB::table('bots')->insert([
                 'name' => "Bot #$i",
                 'description' => "Descripcion para el bot #$i",
@@ -24,7 +27,9 @@ class BotSeeder extends Seeder
                 'username' => "root",
                 'password' => Hash::make('password'),
                 'db_name' => "test_botronic",
-                'user_id' => 2
+                'user_id' => 2,
+                'created_at' => $date,
+                'updated_at' => $date,
             ]);
         }
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -17,19 +18,27 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $date = Carbon::now();
+
         DB::table('users')->insert([
             "name" => "Admin",
             "email" => "admin@admin.com",
             "password" => Hash::make('admin1234'),
-            "type" => 1
+            "type" => 1,
+            "created_at" => $date,
+            "updated_at" => $date,
         ]);
 
         for ($i = 0; $i < 10; $i++) {
+            $date = Carbon::now();
+
             DB::table('users')->insert([
                 "name" => "Test user $i",
                 "email" => "test$i@test.com",
                 "password" => Hash::make('test1234'),
-                "allowed_bots" => rand(1, 7)
+                "allowed_bots" => rand(1, 7),
+                "created_at" => $date,
+                "updated_at" => $date,
             ]);
         }
     }
